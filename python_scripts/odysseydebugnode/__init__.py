7998471a8cfabdc9ac837fcf035ee47e4b16029a
keelson <keelson@tronlab.com>
2017-09-09 17:34:18 +0800

    python script

---
+++ python_scripts/odysseydebugnode/__init__.py
@@ -0,0 +1,3 @@
+from odysseydebugnode.debugnode import DebugNode
+
+__all__ = [ 'debugnode' ]
---
+++ python_scripts/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup( name='odysseydebugnode',
+       version='0.1',
+       description='A wrapper for launching and interacting with a Odyssey Debug Node',
+       url='http://github.com/odyssey/odyssey',
+       author='Odyssey, Inc.',
+       author_email='vandeberg@odyssey.com',
+       license='See LICENSE.md',
+       packages=['odysseydebugnode'],
+       #install_requires=['odysseyapi'],
+       zip_safe=False )
